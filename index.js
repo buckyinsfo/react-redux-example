7e55495c278da19f6ac37c0c3672531eb0d50795
buckyinsfo <buckyinsfo@gmail.com>
2021-02-09 15:37:25 -0800

    Lesson 9 - Single reducer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -28,6 +29,14 @@ const initialState = {
     numOfIceCream: 20
 }
 
+const initialCakeState = {
+    numOfCakes: 10,
+}
+
+const initialIceCreamState = {
+    numOfIceCream: 20
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
@@ -44,10 +53,35 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+const cakeReducer = (state = initialCakeState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
+
+        default: return state
+    }
+}
 
 // *** Our store
 // 1st responsibility
 const store = createStore(reducer)
+// const rootReducer = combineReducers({
+//     cake: cakeReducer, 
+//     iceCream: iceCreamReducer
+// })
+// const store = createStore(rootReducer)
 
 // 2nd respinsibility
 console.log('Initial state', store.getState())
